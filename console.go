2d31039a9d3db5b0e4621173f08bd082f5e5420b
lmittmann <lmittmann@users.noreply.github.com>
2023-09-03 15:04:51 +0200

    refactored console tracer

---
+++ console.go
@@ -0,0 +1,62 @@
+package solc
+
+import (
+	"math/big"
+	"testing"
+
+	"github.com/ethereum/go-ethereum/accounts/abi"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/ethereum/go-ethereum/core/vm"
+	"github.com/lmittmann/solc/internal/console"
+)
+
+// NewConsole returns a new [vm.EVMLogger] that logs calls to console.sol to the
+// given testing.TB.
+func NewConsole(tb testing.TB) vm.EVMLogger {
+	return &consoleTracer{tb: tb}
+}
+
+type consoleTracer struct {
+	tb testing.TB
+}
+
+func (ct *consoleTracer) CaptureEnter(typ vm.OpCode, from common.Address, to common.Address, input []byte, gas uint64, value *big.Int) {
+	if to != console.Addr || len(input) < 4 {
+		return
+	}
+
+	sel := ([4]byte)(input[:4])
+	args, ok := console.Args[sel]
+	if ok {
+		ct.log(args, input[4:])
+	}
+}
+
+func (ct *consoleTracer) log(args abi.Arguments, data []byte) {
+	params, err := args.Unpack(data)
+	if err != nil {
+		ct.tb.Fatalf("malformed log: %v", err)
+		return
+	}
+
+	for i, p := range params {
+		switch p := p.(type) {
+		case []byte:
+			params[i] = hexutil.Bytes(p)
+		}
+	}
+
+	ct.tb.Log(params...)
+}
+
+func (*consoleTracer) CaptureTxStart(uint64) {}
+func (*consoleTracer) CaptureTxEnd(uint64)   {}
+func (*consoleTracer) CaptureStart(*vm.EVM, common.Address, common.Address, bool, []byte, uint64, *big.Int) {
+}
+func (*consoleTracer) CaptureEnd([]byte, uint64, error)  {}
+func (*consoleTracer) CaptureExit([]byte, uint64, error) {}
+func (*consoleTracer) CaptureState(uint64, vm.OpCode, uint64, uint64, *vm.ScopeContext, []byte, int, error) {
+}
+func (*consoleTracer) CaptureFault(uint64, vm.OpCode, uint64, uint64, *vm.ScopeContext, int, error) {
+}
--- internal/console/console.go
+++ internal/console/console.go
@@ -3,13 +3,9 @@ package console
 
 import (
 	_ "embed"
-	"math/big"
-	"testing"
 
 	"github.com/ethereum/go-ethereum/accounts/abi"
 	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/common/hexutil"
-	"github.com/ethereum/go-ethereum/core/vm"
 )
 
 // Source of the console contracts.
@@ -29,51 +25,3 @@ var (
 	argBytes32 = abi.Argument{Type: abi.Type{T: abi.FixedBytesTy, Size: 32}}
 	argBytes   = abi.Argument{Type: abi.Type{T: abi.BytesTy}}
 )
-
-// NewTracer returns a [vm.EVMLogger] that prints debug calls.
-func NewTracer(tb testing.TB) vm.EVMLogger {
-	tb.Helper()
-	return &tracer{tb: tb}
-}
-
-type tracer struct {
-	tb testing.TB
-}
-
-func (t *tracer) CaptureEnter(typ vm.OpCode, from common.Address, to common.Address, input []byte, gas uint64, value *big.Int) {
-	if to != Addr || len(input) < 4 {
-		return
-	}
-
-	sel := ([4]byte)(input[:4])
-	args, ok := Args[sel]
-	if ok {
-		t.logConsole(args, input[4:])
-	}
-}
-
-func (*tracer) CaptureTxStart(uint64) {}
-func (*tracer) CaptureTxEnd(uint64)   {}
-func (*tracer) CaptureStart(*vm.EVM, common.Address, common.Address, bool, []byte, uint64, *big.Int) {
-}
-func (*tracer) CaptureEnd([]byte, uint64, error)  {}
-func (*tracer) CaptureExit([]byte, uint64, error) {}
-func (*tracer) CaptureState(uint64, vm.OpCode, uint64, uint64, *vm.ScopeContext, []byte, int, error) {
-}
-func (*tracer) CaptureFault(uint64, vm.OpCode, uint64, uint64, *vm.ScopeContext, int, error) {}
-
-func (t *tracer) logConsole(args abi.Arguments, data []byte) {
-	params, err := args.Unpack(data)
-	if err != nil {
-		t.tb.Fatalf("malformed log: %v", err)
-	}
-
-	for i, p := range params {
-		switch p := p.(type) {
-		case []byte:
-			params[i] = hexutil.Bytes(p)
-		}
-	}
-
-	t.tb.Log(params...)
-}
